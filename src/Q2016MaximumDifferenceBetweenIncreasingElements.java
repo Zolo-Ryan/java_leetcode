33590a17b4aa4e799dbf62deae6f4ebc8db7b4a7
Zolo-Ryan <utkarshswaroopsri187@gmail.com>
2025-06-17 00:07:40 +0530

    16 potd
    easy h, keep the min element

---
+++ src/Q2016MaximumDifferenceBetweenIncreasingElements.java
@@ -0,0 +1,11 @@
+public class Q2016MaximumDifferenceBetweenIncreasingElements {
+    public int maximumDifference(int[] nums) {
+        int diff = -1;
+        int min = nums[0];
+        for(int i = 1; i < nums.length;i++){
+            if(nums[i] > min) diff = Math.max(diff, nums[i] - min);
+            if(nums[i] < min) min = nums[i];
+        }
+        return diff;
+    }
+}
